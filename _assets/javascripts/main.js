1f66571d5547666ee05d48af8d423a0883f76ab8
Joddsson <ivaroddsson@gmail.com>
2014-11-09 22:17:51 +0000

    Added some pages.

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,3 +1,20 @@
+$(document).ready(function(){
+           $('.row img').on('click',function(){
+                var src = $(this).attr('src');
+                var img = '<img src="' + src + '" class="img-responsive"/>';
+                $('#myModal').modal({
+                	//backdrop: false
+                });
+                $('#myModal').on('shown.bs.modal', function(){
+                    $('#myModal .modal-body').html(img);
+                });
+                $('#myModal').on('hidden.bs.modal', function(){
+                    $('#myModal .modal-body').html('');
+                });
+           });  
+        })
+
 $(".full img").on("click", function() {
   $(this).toggleClass("zoom");
 });
+
