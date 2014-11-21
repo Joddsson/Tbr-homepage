7a36a4d40500680c57eb1793cea90d94f77e9ab1
Joddsson <ivaroddsson@gmail.com>
2014-11-21 02:47:30 +0000

    First iteration complete

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -3,12 +3,17 @@ $(document).ready(function(){
                 var src = $(this).attr('src');
                 var img = '<img src="' + src + '" width="570" height="570"/>';
                 var modal_title = $(this).attr('id');
+                //var modal_footer = $(this).attr('class');
                 $('#myModal').modal({
                 
                 });
                 $('#myModal').on('shown.bs.modal', function(){
                     $('#myModal .modal-title').html(modal_title);
                     $('#myModal .modal-body').html(img);
+                    /*if ('.row img').hasClass("woman_fit")) {
+                        alert("hello!");
+                        $('#myModal .modal-footer').html('<p>Hello</p>');
+                    };*/
                 });
                 $('#myModal').on('hidden.bs.modal', function(){
                     $('#myModal .modal-body').html('');
